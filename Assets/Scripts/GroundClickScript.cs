61a338c25af8cd36b1b0f9f3237def15a38de006
Meg Drouhard <drouhardmg@ornl.gov>
2015-07-02 15:24:28 -0400

    Initial commit - fractal with player exploration

---
+++ Assets/Scripts/GroundClickScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundClickScript : MonoBehaviour {
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+		GameObject road = new GameObject ("road", typeof(MeshFilter), typeof(MeshRenderer));
+		MeshFilter meshFilter = road.GetComponent<MeshFilter>();
+		MeshRenderer meshRenderer = road.GetComponent<MeshRenderer> ();
+
+		Mesh mesh = new Mesh ();
+
+		meshFilter.mesh = mesh;
+	}
+}
